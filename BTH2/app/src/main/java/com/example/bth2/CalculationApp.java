2a67869ba4409f7bc6ee629665b9b1db9a21626b
2151013049linh@ou.edu.vn <2151013049linh@ou.edu.vn>
2024-07-10 11:43:17 +0700

    update

--- BTH2/app/src/main/java/com/example/bth2/CalculationApp.java
+++ BTH2/app/src/main/java/com/example/bth2/CalculationApp.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class CalculationApp extends AppCompatActivity  {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculation);
     }
